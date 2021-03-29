402501a151d06f29568f708686003f07c7f944a4
Pontus Wikholm <pontus@wftech.se>
2021-03-29 23:20:42 +0200

    plan generated

--- Form1.cs
+++ Form1.cs
@@ -43,11 +43,82 @@ namespace DebugSQLStuff
             //Query Input
             String ProjectName = "TopHat_2021-03 DualBoxProj";
             String CustomerName = "TopHat";
+
             String ModuleName = "M1FF";
             DateTime ModuleCreationDate = DateTime.Now;
             Single BoundsLength = 9168;
             Single BoundsWidth = 4472;      //Kalle hur skall jag hantera decimaler efter kommatecken? (skall vara .5 på bredden...)
             Single BoundsHight =2484;
+
+            Single DimTopBottom = 72;
+            Single DimMainStuds = 44;
+            Single DimSecondaryStuds = 72;
+            Single DimStandardSheets = 1200;
+            Single LengthSpecialFirstAndLastSheets = 839;
+            Single NrOfOrdinaryStudPlacesTakenUpBySUBELEMENT = 2;
+            Single ExtraStud1Xpos = 3310;
+            Single ExtraStud2Xpos = 5683;
+            bool EW1GableHasAWindow = true;
+            bool EW1GableHasExtraStuds = true;
+
+            //Board info needed to create a Board!
+            Guid BoardTypeID = Guid.NewGuid();
+            Guid ParentID = Guid.NewGuid();
+            String Description;
+            Single Length;
+            int RotationX;
+            int RotationY;
+            int RotationZ;
+            Single PositionX;
+            Single PositionY;
+            Single PositionZ;
+
+
+            //General rules for EW1 Gable --> Each and every stud is of secondary dimmension except for the first two studs (standard) and the closest studs to the SUBELEMENT is the secondary type.
+
+
+
+            //**** Exception will be that one stud in the real data from TopHat in EW1 Gable does not apply to the each and every rule. ****//
+
+            // 1. Create first main stud                                                            [44]
+            //      1.1 Xpos --> 0
+            // 2. Create second main stud                                                           [44]
+            //      2.1 Xpos --> 1.1 Xpos + DimLastStud/2 + DimStandardSheets/2 - DimThisStud/2
+            // 3. Create final stud att end of LengthSpecialFirstAndLastSheets                      [72]
+            //      3.1 Xpos --> LengthSpecialFirstAndLastSheets - DimThisStud/2
+            // 4. Create first stud under standard sheets                                           [44]
+            //      4.1 XPos --> 3.1 Xpos + DimLastStud/2 + DimStandardSheets/2 - DimThisStud/2
+            // 5. Create next stud                                                                  [72]
+            //      5.1 XPos --> 4.1 Xpos + DimLastStud/2 + DimStandardSheets/2 - DimThisStud/2
+            // 6. Create next stud                                                                  [44]    <---------- Exception mentioned above
+            //      6.1 XPos --> 5.1 Xpos + DimLastStud/2 + DimStandardSheets/2 - DimThisStud/2
+            // 7. Create next stud                                                                  [72]   
+            //      7.1 XPos --> 6.1 Xpos + DimLastStud/2 + DimStandardSheets/2 - DimThisStud/2
+            // 8. Create next stud                                                                  [44]   
+            //      8.1 XPos --> 7.1 Xpos + DimLastStud/2 + DimStandardSheets/2 - DimThisStud/2
+            // 9. Create next stud                                                                  [72]   
+            //      9.1 XPos --> 8.1 Xpos + DimLastStud/2 + (3 * DimStandardSheets/2) - DimThisStud/2
+            // 10. Create next stud                                                                 [44]   
+            //      10.1 XPos --> 9.1 Xpos + DimLastStud/2 + DimStandardSheets/2 - DimThisStud/2
+            // 11. Create next stud                                                                 [72]   
+            //      11.1 XPos --> 10.1 Xpos + DimLastStud/2 + DimStandardSheets/2 - DimThisStud/2
+            // 12. Create next stud                                                                 [44]   
+            //      12.1 XPos --> 11.1 Xpos + DimLastStud/2 + DimStandardSheets/2 - DimThisStud/2
+            // 13. Create next stud                                                                 [72]   
+            //      13.1 XPos --> 12.1 Xpos + DimLastStud/2 + DimStandardSheets/2 - DimThisStud/2
+            // 14. Create next stud                                                                 [44]   
+            //      14.1 XPos --> 13.1 Xpos + DimLastStud/2 + (LengthSpecialFirstAndLastSheets - DimStandardSheets/2 - DimThisStud)
+            // 15. Create next stud                                                                 [44]   
+            //      15.1 XPos --> 14.1 Xpos + DimLastStud/2 + DimStandardSheets/2 - DimThisStud/2
+            // 16. Create extra stud
+            //      16.1 XPos --> ExtraStud1Xpos
+            // 17. Create extra stud
+            //      17.1 XPos --> ExtraStud2Xpos
+
+            while (true)
+            {
+
+            }
 
             //Query Output
             Guid guidProjId = new Guid();
